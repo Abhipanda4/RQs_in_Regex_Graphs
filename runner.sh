5159b4143b1ea02a6da25f655bb20c3bd4ca1e3d
abhipanda4 <abhipanda03@gmail.com>
2018-04-09 16:56:07 +0530

    runner for all 3 algorithms

--- runner.sh
+++ runner.sh
@@ -3,10 +3,10 @@
 echo "Making binary files for each of the 3 methods..."
 g++ -std=c++11 ./src/trans_closure.cpp -o ./src/trans_closure
 g++ -std=c++11 ./src/partial_indexing_BFS.cpp -o ./src/partial_indexing_BFS
-#g++ -std=c++11 ./src/BFS_efficient.cpp -o ./src/BFS_efficient
+g++ -std=c++11 ./src/BFS_efficient.cpp -o ./src/BFS_efficient
 echo "Binary files successfully created!"
 
-if [ -a "data_files/data_algo1.csv" ]
+if [ -a "data_files/data_algo2.csv" ]
 then
     echo "Removing old data files"
     rm data_files/data_algo*
@@ -24,8 +24,8 @@ do
     du -h ../BFS_index/trans_closure >> ../data_files/index_sizes
     echo -e "\t-> Using Partial Indexing and BFS algorithm"
     ./partial_indexing_BFS ../data_files/graph_$N.txt >> ../data_files/data_algo2.csv
-    #echo -e "\t-> Using only BFS algorithm"
-    #./BFS_efficient ../data_files/graph_$N.txt >> ../data_files/data_algo3.csv
+    echo -e "\t-> Using only BFS algorithm"
+    ./BFS_efficient ../data_files/graph_$N.txt >> ../data_files/data_algo3.csv
 done
 
 echo "Cleaning up ..."
